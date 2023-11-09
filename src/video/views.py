ea9303f4b8f68b4141f373ab97d5d11a4d3faf6e
SirAbhi13 <abhinav.sny.2002@gmail.com>
2023-11-10 02:01:16 +0530

    latest changes

--- src/video/views.py
+++ src/video/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from rest_framework import status
+from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -9,31 +10,26 @@ from .serializers import VideoSerializer
 
 
 class AudioExtractionView(APIView):
+    # parser_classes = [FileUploadParser]
     def post(self, request):
-        serializer = VideoSerializer(data=request.data)
+        # serializer = VideoSerializer(data={"video_file": request.FILES['video_file']})
+        # serializer = VideoSerializer(data=request.data)
 
-        if serializer.is_valid():
-            # video_file = serializer.validated_data["video_file"]
-            return Response({"detail": "succes"}, status=status.HTTP_200_OK)
+        # if serializer.is_valid():
 
-            # Implement FFmpeg logic for audio extraction here
-            # Extract audio from video_file and save it to an audio file
+        # Implement FFmpeg logic for audio extraction here
+        # Extract audio from video_file and save it to an audio file
 
-            # Assuming you've extracted audio and saved it as audio_file, you can then create a record in the database
-            # audio_file = "path/to/extracted/audio.wav"
-            # video = Video.objects.create(
-            #     username=request.user.username,  # Assuming user is authenticated
-            #     name=video_file.name,
+        # Assuming you've extracted audio and saved it as audio_file, you can then create a record in the database
+        # audio_file = "path/to/extracted/audio.wav"
+        # print(serializer.data)
+        video = Video.objects.create(
+            user="tester",  # Assuming user is authenticated
+            video_file=request.FILES["video_file"],
+        )
 
-            #     size=video_file.size,
-            #     uploaded_at=datetime.now(),
-            #     file=audio_file,
-            # )
-
-            # return Response(
-            #     {"audio_url": audio_file.url}, status=status.HTTP_201_CREATED
-            # )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"audio_url": "succes"}, status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
         return Response({"detail": "succes"}, status=status.HTTP_200_OK)
