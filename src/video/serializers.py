e545e754d5065004fb354732b5dec6931dcc8019
SirAbhi13 <abhinav.sny.2002@gmail.com>
2023-11-10 09:10:07 +0530

    serializer works now

--- src/video/serializers.py
+++ src/video/serializers.py
@@ -4,9 +4,9 @@ from rest_framework import serializers
 
 class VideoSerializer(serializers.Serializer):
     video_file = serializers.FileField(
-        # validators=[
-        #     FileExtensionValidator(
-        #         allowed_extensions=["mp4", "avi", "mkv", "mov", "wmv"]
-        #     ),
-        # ]
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=["mp4", "avi", "mkv", "mov", "wmv"]
+            ),
+        ]
     )
--- src/video/views.py
+++ src/video/views.py
@@ -13,23 +13,23 @@ class AudioExtractionView(APIView):
     # parser_classes = [FileUploadParser]
     def post(self, request):
         # serializer = VideoSerializer(data={"video_file": request.FILES['video_file']})
-        # serializer = VideoSerializer(data=request.data)
+        serializer = VideoSerializer(data=request.data)
 
-        # if serializer.is_valid():
+        if serializer.is_valid():
+            # create a video object
+            video = Video.objects.create(
+                user="tester",  # Assuming user is authenticated
+                video_file=serializer.validated_data["video_file"],
+            )
 
-        # Implement FFmpeg logic for audio extraction here
-        # Extract audio from video_file and save it to an audio file
+            # Implement FFmpeg logic for audio extraction here
+            # Extract audio from video_file and save it to an audio file
 
-        # Assuming you've extracted audio and saved it as audio_file, you can then create a record in the database
-        # audio_file = "path/to/extracted/audio.wav"
-        # print(serializer.data)
-        video = Video.objects.create(
-            user="tester",  # Assuming user is authenticated
-            video_file=request.FILES["video_file"],
-        )
+            # Assuming you've extracted audio and saved it as audio_file, you can then create a record in the database
+            # audio_file = "path/to/extracted/audio.wav"
 
-        return Response({"audio_url": "succes"}, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"audio_url": "succes"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
         return Response({"detail": "succes"}, status=status.HTTP_200_OK)
