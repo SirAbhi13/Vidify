e253e999d2145c594145d3c494d4a03caf92a8c0
SirAbhi13 <abhinav.sny.2002@gmail.com>
2023-11-10 11:02:28 +0530

    audio extraction working

--- src/video/views.py
+++ src/video/views.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from datetime import datetime
 
 from rest_framework import status
@@ -21,15 +23,24 @@ class AudioExtractionView(APIView):
                 user="tester",  # Assuming user is authenticated
                 video_file=serializer.validated_data["video_file"],
             )
+            audio_path = self.extract_audio(video.video_file.path)
 
             # Implement FFmpeg logic for audio extraction here
             # Extract audio from video_file and save it to an audio file
 
             # Assuming you've extracted audio and saved it as audio_file, you can then create a record in the database
             # audio_file = "path/to/extracted/audio.wav"
-
             return Response({"audio_url": "succes"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
         return Response({"detail": "succes"}, status=status.HTTP_200_OK)
+
+    def extract_audio(self, video_path):
+        audio_path = (
+            f"src/video/audios/audio_{os.path.basename(video_path).split('.')[0]}.mp3"
+        )
+        subprocess.run(
+            ["ffmpeg", "-i", video_path, "-q:a", "0", "-map", "a", audio_path]
+        )
+        return audio_path
